32ab3f6c2f0871b5db5f9668325a5e77a20e38ce
JoseQuintas <jmcquintas@gmal.com>
2025-08-01 13:19:46 -0300

    IE  PA

--- source/ze_validiepa.sql
+++ source/ze_validiepa.sql
@@ -9,13 +9,13 @@ BEGIN
 DECLARE lOk INT(11);
 DECLARE nLen INT(11);
 
-SET cInscricao = ze_SoNumeros( cInscricao );
+SET cInscricao = ze_SoNumero( cInscricao );
 
 IF LENGTH( cInscricao ) <> 9 THEN
    RETURN 0;
 END IF;
 
-IF SUBSTR( cInscricao, 1, 2 ) <> '15' THEN
+IF ! SUBSTR( cInscricao, 1, 2 ) IN ( '15', '75', '76', '77', '78', '79' ) THEN
    RETURN 0;
 END IF;
 IF ze_CalculaDigito( SUBSTR( cInscricao, 1, 8 ), '11' ) <> SUBSTR( cInscricao, 9, 1 ) THEN
